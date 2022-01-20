bf60d8f6c73044fc22ae96c14b1358fdd18337f6
weiwujing <kenquiet@163.com>
2022-01-20 10:05:28 +0800

    1

--- src/com/xwz/transaction/service/AccountService.java
+++ src/com/xwz/transaction/service/AccountService.java
@@ -1,13 +1,12 @@
 package com.xwz.transaction.service;
 
 import com.xwz.transaction.dao.Account;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@Transactional
+@Transactional // 开启事务
 public class AccountService {
   @Autowired
   public Account account;
